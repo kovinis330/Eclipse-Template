e268016e2b409c60deca92f17f3460104e3dc4a8
kovinis <v.jacevicius@gmail.com>
2016-12-15 13:39:43 +0200

    finalizing style

---
+++ scripts/custom.js
@@ -0,0 +1,11 @@
+// var a = 2,
+//     b = 3;
+//
+// if (a > b) {
+//   console.log("Hello, World!")
+// } else {
+//   console.log("Goodbye, World!")
+// }
+//
+// var aa = 12.15;
+// console.log(aa.toPrecision(3));
