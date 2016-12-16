020b74ecc5e338ef34b03bdae8642ab88b0e4d4d
kovinis <v.jacevicius@gmail.com>
2016-12-16 15:34:45 +0200

    added responsive navigation

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,11 +1,5 @@
-// var a = 2,
-//     b = 3;
-//
-// if (a > b) {
-//   console.log("Hello, World!")
-// } else {
-//   console.log("Goodbye, World!")
-// }
-//
-// var aa = 12.15;
-// console.log(aa.toPrecision(3));
+$(document).ready(function(){
+    $(".burger-container").on('click', function(){
+        $(this).toggleClass("open");
+    });
+});
